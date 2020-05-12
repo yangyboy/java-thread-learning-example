534fea81573a4023554fafd865a052b33ab2d7c4
杨洋 <yangyboy@163.com>
2020-05-12 21:30:02 +0800

    初始化提交

---
+++ src/test/java/com/mocc/thread/core/ThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocc.thread.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
